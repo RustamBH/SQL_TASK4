4f4965bb94ae6d23a5ff48b1bbc7d0b9f1dd4421
RustamBH <kamaji@mail.ru>
2022-04-13 22:53:04 +0400

    Added SQL files

---
+++ SELECT_task.sql
@@ -0,0 +1,33 @@
+название и год выхода альбомов, вышедших в 2018 году;
+SELECT name, release_date
+FROM album
+WHERE release_date = 2018
+
+
+название и продолжительность самого длительного трека;
+SELECT name, duration
+FROM tracks
+where duration = (select MAX(duration) from tracks);
+
+
+название треков, продолжительность которых не менее 3,5 минуты;
+SELECT name, duration
+FROM tracks
+WHERE duration > 3.5 * 60;
+
+
+названия сборников, вышеSELECT name
+FROM collection
+WHERE release_date BETWEEN 2018 AND 2020;дших в период с 2018 по 2020 год включительно;
+
+
+исполнители, чье имя состоит из 1 слова;
+SELECT name
+FROM singers
+WHERE name NOT LIKE '% %';
+
+
+название треков, которые содержат слово "мой"/"my".
+SELECT name
+FROM tracks
+WHERE name LIKE '%мой%' OR name LIKE '%my%';
